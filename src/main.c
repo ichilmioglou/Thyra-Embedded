7a562aad918b0b7171a5f5e93f5201e7d474646e
ilker <ilkerxlm@gmail.com>
2025-02-01 20:19:30 +0100

    hello world

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+
+#define LED1_NODE DT_NODELABEL(led1)
+#define BUTTON1_NODE DT_NODELABEL(button1)
+
+int main(void)
+{
+        static const struct gpio_dt_spec led1_dev = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+        static const struct gpio_dt_spec button1_dev = GPIO_DT_SPEC_GET(BUTTON1_NODE, gpios);
+
+        gpio_pin_configure_dt(&led1_dev, GPIO_OUTPUT);
+        gpio_pin_configure_dt(&button1_dev, GPIO_INPUT);
+        for (;;)
+        {
+                bool button1_pressed = gpio_pin_get_dt(&button1_dev);
+                gpio_pin_set_dt(&led1_dev, button1_pressed);
+                k_msleep(50);
+        }
+        return 0;
+}
